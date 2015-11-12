4f20b4a00fbb7cec4c4db1937bec6a87363f045c
ReesKlintworth <reesk5150@gmail.com>
2015-11-11 22:13:46 -0600

    Add run queue prune and prioritize functions.
    Everything compiles and is so very happy.

--- project2-thread-scheduler/hello_world.c
+++ project2-thread-scheduler/hello_world.c
@@ -16,37 +16,37 @@
 // compute the number of alarm ticks relative to system ticks per second
 #define ALARMTICKS(x) ((alt_ticks_per_second()*(x))/10)
 #define NUM_THREADS 12
+#define MAX 10000
 
-struct tcb {
+typedef enum {
+	scheduled,
+	running,
+	finished
+} thread_status;
+
+typedef struct tcb {
 	int thread_id;
 	int run_count;
 	thread_status status;
 	void* context;
-};
-
-enum thread_status
-{
-	scheduled,
-	running,
-	finished
-};
+} tcb;
 
 // entry point to prototype operating system
 void prototype_os();
-
 void mythread(int thread_id);
-
-tcb * mythread_create(int thread_id);
-
+void mythread_create(int thread_id, tcb *instance);
 void * mythread_scheduler(void *context);
-
 // callback function for alarm interrupt
 alt_u32 interrupt_handler(void* context);
+// helper function for pruning run queue of completed threads
+void prune_queue();
+// helper function for prioritizing threads in run queue
+void prioritize_queue();
 
 // the alarm that will regularly interrupt program execution
 alt_alarm alarm;
 
-int run_queue[NUMTHREADS];
+tcb *run_queue[NUM_THREADS];
 int run_queue_count;
 
 int main()
@@ -62,22 +62,21 @@ void mythread(int thread_id)
 	n = (thread_id % 2 == 0) ? 10 : 15;
 	for (i = 0; i < n; i++)
 	{
-		printf("This is message %d of thread # %d.\n", i, thread_id);
+		alt_printf("This is message %d of thread # %d.\n", i, thread_id);
 		for (j = 0; j < MAX; j++);
 	}
 }
 
-tcb * mythread_create(int thread_id)
+void mythread_create(int thread_id, tcb *instance)
 {
-	// TODO: Free bird!
 	tcb *thread = (tcb *)malloc(sizeof(tcb));
 	thread->thread_id = thread_id;
 	// add thread to run_queue
 	run_queue[thread_id] = thread;
 	run_queue_count++;
 	// thread has been added to queue; set its status to scheduled
-	thread->status = thread_status.scheduled;
-	return &thread;
+	thread->status = scheduled;
+	instance = thread;
 }
 
 void * mythread_scheduler(void *context)
@@ -86,12 +85,71 @@ void * mythread_scheduler(void *context)
 	if(run_queue_count > 0)
 	{
 		// suspend the current thread and schedule a new thread
+
+		// remove completed threads from the queue
+		prune_queue();
+		// reprioritize the queue
+		prioritize_queue();
+
+		// schedule the new highest priority thread
 	}
 	else
 	{
-		alt.printf("Interrupted by the DE2 timer!\n");
+		alt_printf("Interrupted by the DE2 timer!\n");
 	}
 	// do whatever we need to do
+}
+
+void prune_queue()
+{
+	int i, j;
+	// iterate through queue
+	for (i = 0; i < NUM_THREADS; i++)
+	{
+		tcb *thread = run_queue[i];
+		// thread has finished and exited
+		if (thread->status == finished)
+		{
+			// remove the completed thread from the queue
+			run_queue[i] = NULL;
+			run_queue_count--;
+			// destroy the thread
+			free(thread);
+			// shift all remaining threads in the queue up
+			for(j = i; j < NUM_THREADS-1; j++)
+			{
+				run_queue[j] = run_queue[j+1];
+			}
+			// insert an empty slot at the end of the queue
+			run_queue[NUM_THREADS-1] = NULL;
+		}
+	}
+}
+
+void prioritize_queue()
+{
+	// determine how many open spots are available in the queue
+	// snapshot the current highest priority thread
+	tcb *current_thread = run_queue[0];
+	if (current_thread == NULL)
+	{
+		alt_printf("run queue is empty");
+		return;
+	}
+	// make the highest priority slot available
+	run_queue[0] = NULL;
+	int i;
+	for(i = 0; i < NUM_THREADS-1; i++)
+	{
+		// the rest of the queue is empty slots
+		if (run_queue[i] == NULL)
+		{
+			break;
+		}
+		run_queue[i] = run_queue[i+1];
+	}
+	// insert the former highest priority thread at the back of the queue
+	run_queue[i] = current_thread;
 }
 
 void prototype_os()
@@ -123,7 +181,6 @@ void prototype_os()
 
 alt_u32 interrupt_handler(void* context)
 {
-	global_flag = 1;
 	alt_printf("Interrupted by timer!\n");
 	// reset the alarm to interrupt next in 0.5 seconds
 	return ALARMTICKS(5);
